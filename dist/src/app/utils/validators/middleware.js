5f5d1e054f268c44b1df6a8675d5d71c0ac035b5
Pedro Venâncio <ph.venancio2@gmail.com>
2024-05-23 21:19:46 -0300

    debug: middleware

--- dist/src/app/utils/validators/middleware.js
+++ dist/src/app/utils/validators/middleware.js
@@ -4,6 +4,7 @@ const tokenValid_1 = require("./tokenValid");
 function middleware(req, res) {
     const token = req.headers.authorization;
     const tokenIsValid = (0, tokenValid_1.default)(token);
+    console.log(`Token: ${token}\nToken is valid: ${tokenIsValid}`);
     if (!token || !tokenIsValid)
         return res.status(401).json({ error: 'Unauthorized' });
 }
--- src/app/utils/validators/middleware.ts
+++ src/app/utils/validators/middleware.ts
@@ -4,5 +4,9 @@ import tokenValid from "./tokenValid";
 export default function middleware(req: Request, res: Response) {
     const token = req.headers.authorization;
     const tokenIsValid = tokenValid(token as string);
+    console.log(
+        `Token: ${token}\nToken is valid: ${tokenIsValid}`
+    );
+    
     if (!token || !tokenIsValid) return res.status(401).json({error: 'Unauthorized'});
 }
