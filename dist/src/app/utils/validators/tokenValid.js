2592d8e91bbdbb78c0972618846641e84f8fdd85
Pedro Venâncio <ph.venancio2@gmail.com>
2024-05-23 21:07:23 -0300

    debug: middleware

--- dist/src/app/utils/validators/tokenValid.js
+++ dist/src/app/utils/validators/tokenValid.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 function tokenValid(token) {
+    console.log('tokenValid', token, process.env.TOKEN_SECRET);
     return token === process.env.TOKEN_SECRET;
 }
 exports.default = tokenValid;
--- src/app/utils/validators/tokenValid.ts
+++ src/app/utils/validators/tokenValid.ts
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 export default function tokenValid(token: string): boolean {
+    console.log('tokenValid', token, process.env.TOKEN_SECRET);
     return token === process.env.TOKEN_SECRET;
 }
